a92f01a8a7438b37ec453d6543ad98e178699492
Devanik Debnath <162272415+Devanik21@users.noreply.github.com>
2025-04-30 20:16:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ with st.sidebar:
         st.session_state.api_key = saved_api_key
     
     model_name = st.selectbox("Select AI Model", 
-                             ["gemini-2.0-flash","gemini-2.5-flash","gemini-2.5-pro" "gemini-2.0-flash-lite", "gemini-2.0-pro-exp-02-05", 
+                             ["gemini-2.0-flash","gemini-2.5-flash-preview-04-17","gemini-2.5-pro-preview-03-25" "gemini-2.0-flash-lite", "gemini-2.0-pro-exp-02-05", 
                               "gemini-2.0-flash-thinking-exp-01-21", "gemini-1.5-pro", 
                               "gemini-1.5-flash", "gemini-1.5-flash-8b"], 
                              index=0)
